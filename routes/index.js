34d58a28a1d2e4a5754140ed7d2183fad164e6cf
99xiaolang <lab17737886183@163.com>
2021-01-22 17:02:54 +0800

    页面构建:登录,注册,首页完成(分页,创作中心,详情未完成)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// 首页
+router.get('/', function(req, res) {
+  res.render('index', {});
 });
-
+// 登录
+router.get('/login', function(req, res) {
+  res.render('login', {});
+});
+// 注册
+router.get('/signIn', function(req, res) {
+  res.render('signIn', {});
+});
+//创作中心
+router.get('/write', function(req, res) {
+  res.render('write', {});
+});
+//详情页
+router.get('/blokInfo', function(req, res) {
+  res.render('blokInfo', {});
+});
 module.exports = router;
