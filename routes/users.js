3d8c54db3933df24f839caa2e396697bc568e49b
99xiaolang <lab17737886183@163.com>
2021-01-23 17:10:32 +0800

    页面构建完成，注册可以向数据库提交数据

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
+// 将用户模板导入
+let User = require('../models/user')
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/addUser',(req,res,next) =>{
+//向数据库添加用户信息
+let userInfo = {
+  userName:req.body.userName,
+  password:req.body.password,
+  Cpassword:req.body.Cpassword,
+}
+
+//用户验证
+if(userInfo.password != userInfo.Cpassword){
+  let error = {
+    status:0,
+    stack:''
+  }
+  res.render('error',{error,message:'密码不一致'})
+}
+
+//页面表单数据 放入模型
+let userI = new User(userInfo)
+
+//保存
+userI.save((err,result) => {
+  if(!err){
+    res.send(result)
+  }
+})
+
+
+  console.log(req.body);
+})
+
 
 module.exports = router;
